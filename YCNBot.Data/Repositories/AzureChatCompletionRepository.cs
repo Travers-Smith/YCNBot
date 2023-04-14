473283e7975ae6930af24ddad81a0a383c57134e
Sam Lansley <Sam.Lansley@traverssmith.com>
2023-04-14 09:12:02 +0100

    Added ApiVersion for Azure to configuration file

--- YCNBot.Data/Repositories/AzureChatCompletionRepository.cs
+++ YCNBot.Data/Repositories/AzureChatCompletionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.Configuration;
+using System.Text;
 using System.Text.Json;
 using YCNBot.Core.Entities;
 using YCNBot.Core.Repositories;
@@ -8,16 +9,17 @@ namespace YCNBot.Data.Repositories
     public class AzureChatCompletionRepository : IChatCompletionRepository
     {
         private readonly HttpClient _httpClient;
-
-        public AzureChatCompletionRepository(HttpClient httpClient)
+        private readonly IConfiguration _configuration;
+        public AzureChatCompletionRepository(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
 
         public async Task<ChatCompletion> CompleteChat(AddChatCompletionServiceModel addChatCompletion)
         {
             HttpResponseMessage response = await _httpClient.PostAsync(
-                "deployments/chatgpt3/chat/completions?api-version=2023-03-15-preview",
+                "deployments/chatgpt3/chat/completions?api-version=" + _configuration["AzureApiVersion"],
                 new StringContent(JsonSerializer.Serialize(addChatCompletion), Encoding.UTF8, "application/json"));
 
             response.EnsureSuccessStatusCode();
--- YCNBot.Data/UnitOfWork.cs
+++ YCNBot.Data/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using YCNBot.Data.Repositories;
 using YCNBot.Core;
 using YCNBot.Core.Repositories;
+using Microsoft.Extensions.Configuration;
 
 namespace YCNBot.Data
 {
@@ -9,6 +10,7 @@ namespace YCNBot.Data
         private IChatRepository? _chat;
         private IChatCompletionRepository _azureOpenAIChatCompletion;
         private IChatCompletionRepository _openAIChatCompletion;
+        private IConfiguration _configuration;
         private IMessageRepository? _message;
         private IUserAgreedTermsRepository? _userAgreedTerms; 
 
@@ -16,14 +18,15 @@ namespace YCNBot.Data
         private readonly HttpClient _openAIClient;
         private readonly HttpClient _azureOpenAIClient;
 
-        public UnitOfWork(YCNBotContext context, IHttpClientFactory httpClientFactory)
+        public UnitOfWork(IConfiguration configuration, YCNBotContext context, IHttpClientFactory httpClientFactory)
         {
+            _configuration = configuration;
             _context = context;
             _azureOpenAIClient = httpClientFactory.CreateClient("AzureOpenAIClient");
             _openAIClient = httpClientFactory.CreateClient("OpenAIClient");
         }
 
-        public IChatCompletionRepository AzureOpenAIChatCompletion => _azureOpenAIChatCompletion ??= new AzureChatCompletionRepository(_azureOpenAIClient);
+        public IChatCompletionRepository AzureOpenAIChatCompletion => _azureOpenAIChatCompletion ??= new AzureChatCompletionRepository(_azureOpenAIClient, _configuration);
 
         public IChatRepository Chat => _chat ??= new ChatRepository(_context);
 
